0abf76c7b70b065b266922d109351862db674dc0
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-17 17:43:13 -0300

    implements CreateOrderService

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -24,7 +24,7 @@ class OrdersRepository implements IOrdersRepository {
 
   public async findById(id: string): Promise<Order | undefined> {
     const order = this.ormRepository.findOne(id, {
-      relations: ['order_product', 'customer'],
+      relations: ['order_products', 'customer'],
     });
 
     return order;
